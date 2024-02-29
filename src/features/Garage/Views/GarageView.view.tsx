5afeed7d43ffd1e21fb8b7d9047d42ad1b75029e
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-02-29 10:19:08 +0100

    changed text to swedish

--- src/features/Garage/Views/GarageView.view.tsx
+++ src/features/Garage/Views/GarageView.view.tsx
@@ -15,8 +15,8 @@ export const GarageView: FC = () => {
       <Hero />
       <BackgroundgImageSection parralax={true} image_path='/images/view.png'>
         <DisplayInfoWithTitle
-          title='Were we all meet'
-          text='Welcome to our vibrant restaurant, where the atmosphere is as inviting as the cuisine is delectable. Step into a haven where friends gather, savory aromas fill the air, and every bite tells a story of culinary craftsmanship. Our establishment is more than just a place to dine; its a community hub where people come together to share laughter, indulge in delicious meals, and savor the joy of good company. Immerse yourself in the soothing melodies of carefully selected music, creating a backdrop for conversations and connections. Leave your worries at the door as you unwind, relishing an experience that transcends the ordinary and transforms every meal into a memorable occasion.'
+          title='Där vi alla möts'
+          text='Välkommen till vår livliga restaurang, där atmosfären är lika inbjudande som maten är läcker. Kliv in i en fristad där vänner samlas, läckra dofter fyller luften och varje tugga berättar en historia om kulinariskt hantverk. Vår etablering är mer än bara en plats att äta på; det är en samlingspunkt där människor möts för att dela skratt, njuta av läckra måltider och uppskatta glädjen av gott sällskap. Fördjupa dig i de lugnande melodierna från noggrant utvald musik, som skapar en bakgrund för samtal och relationer. Lämna dina bekymmer vid dörren när du kopplar av och njuter av en upplevelse som överträffar det vanliga och förvandlar varje måltid till en minnesvärd tillställning.'
         />
       </BackgroundgImageSection>
       <ImageScrollView />
