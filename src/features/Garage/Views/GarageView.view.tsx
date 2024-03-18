cadd32e6e8357f31eedf7f1dd49d34b902337b0c
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-03-18 16:20:38 +0100

    changed title

--- src/features/Garage/Views/GarageView.view.tsx
+++ src/features/Garage/Views/GarageView.view.tsx
@@ -14,7 +14,7 @@ export const GarageView: FC = () => {
       <Hero />
       <BackgroundgImageSection parralax={true} image_path='/images/img5.webp'>
         <DisplayInfoWithTitle
-          title='Där vi alla möts'
+          title='Hunker down'
           text='Välkommen till vår livliga restaurang, där atmosfären är lika inbjudande som maten är läcker. Kliv in i en fristad där vänner samlas, läckra dofter fyller luften och varje tugga berättar en historia om kulinariskt hantverk. Vår etablering är mer än bara en plats att äta på; det är en samlingspunkt där människor möts för att dela skratt, njuta av läckra måltider och uppskatta glädjen av gott sällskap. Fördjupa dig i de lugnande melodierna från noggrant utvald musik, som skapar en bakgrund för samtal och relationer. Lämna dina bekymmer vid dörren när du kopplar av och njuter av en upplevelse som överträffar det vanliga och förvandlar varje måltid till en minnesvärd tillställning.'
         />
       </BackgroundgImageSection>
