79fe6e8f578fcc3d4ce316bbc4a4a56b2350c7ec
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-03-20 17:41:45 +0100

    cheanged missspelling

--- src/features/About/components/AboutTextContainer/AboutTextContainer.component.tsx
+++ src/features/About/components/AboutTextContainer/AboutTextContainer.component.tsx
@@ -20,7 +20,7 @@ export const AboutTextContainer: FC = () => {
         <PrimaryText text='Emile och hans vänner är inte bara passionerade matentusiaster, de är även skaparna bakom varje detalj på Hunker Junker. Från restaurangens design till menyutformningen har de byggt allt från grunden med sina egna händer och idéer. Deras engagemang och kreativitet genomsyrar varje hörn av restaurangen och skapar en unik atmosfär. Att vara sina egna skapare ger dem en djup känsla av stolthet och tillfredsställelse när de ser deras dröm förverkligas varje dag. För Emile och hans partner är Hunker Junker inte bara en restaurang, det är en manifestation av deras passion och arbete.' />
       </TextContainer>
       <TextContainer>
-        <SecondaryTitle>PEROSNALEN OCH GÄSTERNA?</SecondaryTitle>
+        <SecondaryTitle>PERSONALEN OCH GÄSTERNA?</SecondaryTitle>
         <PrimaryText text='Vi är det vi är tack vare våra älskade gäster och personal. Deras närvaro och engagemang formar Hunker Junker till det vi älskar mest. En plats där vi alla kan mötas och umgås tillsamans.' />
       </TextContainer>
     </Container>
