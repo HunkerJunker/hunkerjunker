75c8819d1b694e1f03e2d8ec504b963d77fe3fdc
Nils Fält <nilsfalt@Nilss-MacBook-Pro.local>
2024-06-19 19:25:14 +0200

    up

--- src/data/menus/hunker/menu.data.ts
+++ src/data/menus/hunker/menu.data.ts
@@ -2,67 +2,67 @@ export const hunkerMenuSnacks = [
   {
     title: "FRITTO MISTO VEGGIES 159KR",
     text: "Sommargrönsaker friterade i smet på bubbelvin och vodka. Grillade vaxbönor. Dip.",
-    obsTitle: "",
+    obsTitle: "MINDRE RÄTT",
   },
   {
     title: "SEAFOOD BAKE HOBO-PACK 169 KR",
     text: "Foliepaket med skaldjur, chistorra korv, majs, potatis, smör, godsaker och örter.",
-    obsTitle: "",
+    obsTitle: "MINDRE RÄTT",
   },
   {
     title: "SMASHED POTATOES 139 KR",
-    text: "Friterade smashed potatoes – Manchego foam – inlagd schaloʓenlök, jalapeño. Chistorra smör. Lagrad ost.",
-    obsTitle: "",
+    text: "Friterade smashed potatoes – Manchego foam – inlagd schalotenlök, jalapeño. Chistorra smör. Lagrad ost.",
+    obsTitle: "MINDRE RÄTT",
   },
   {
     title: "HALIBUT ALMOST CRUDO 169 KR",
-    text: "Gravad hälleflundra på peruanskt vis med Yellow- Submarine salsa.",
-    obsTitle: "",
+    text: "Gravad hälleflundra på peruanskt vis med Yellow-Submarine salsa.",
+    obsTitle: "MINDRE RÄTT",
   },
   {
     title: "GRILLED ENDZ 159 KR",
-    text: "Pork belly grillad på speʓ. Orange salsa på grillad paprika och ingefära. Grillad mayo. Grillad giardiniera.",
-    obsTitle: "",
+    text: "Pork belly grillad på spett. Orange salsa på grillad paprika och ingefära. Grillad mayo. Grillad giardiniera.",
+    obsTitle: "MINDRE RÄTT",
   },
   {
     title: "GAMBAS 159 KR",
     text: "Stora räkor stekta i vrålhet stekpanna med chili, vitlök och vin. Bröd.",
-    obsTitle: "",
+    obsTitle: "MINDRE RÄTT",
   },
 ];
 export const hunkerMenu = [
   {
     title: "IBERICO PORK NECK AL PASTOR 299 KR",
     text: "Nacke av Iberico-gris från grillen med mexikanska influenser. Salsa verde (tomatillo) – chistorra-smör – grillad inlagd ananas – friterad potatis – grillad mayo – lime – örter.",
-    obsTitle: "",
+    obsTitle: "FULL SIZE",
   },
   {
     title: "MACKEREL ON THE BARBEEH 299 KR",
     text: "Grillad hel makrill – grillad rökig tomachurri (chimichurri med grillad tomat och paprika) – smör med läʓrökt vitlök – grillat bröd – grillad citron.",
-    obsTitle: "",
+    obsTitle: "FULL SIZE",
   },
   {
     title: "FRIEED SMALL CHICKEN 299 KR",
     text: "Hel vårkyckling stekt under tegelsten i smör – ragu på potatis, inlagd citron, smörbönor, smålök, sparris, oliver, örter – grillad mayo – salsa på paprika, ingefära och morot.",
-    obsTitle: "",
+    obsTitle: "FULL SIZE",
   },
   {
     title: "CELERIAC ON THE BARBEEH 249 KR",
     text: "Nacke av Iberico-gris från grillen med mexikanska influenser. Salsa verde (tomatillo) – chistorra-smör – grillad inlagd ananas – friterad potatis – grillad mayo – lime – örter.",
-    obsTitle: "",
+    obsTitle: "FULL SIZE",
   },
 ];
 export const hunkerMenuKidz = [
   {
     title: "PANCAKES 99 KR",
     text: "Pannkakor med grillad fruktsallad och grädde.",
-    obsTitle: "",
+    obsTitle: "FÖR BARN",
   },
 ];
 export const hunkerMenuDesert = [
   {
     title: "DESSERT 139 KR",
     text: "Faʓig riddare på brioche pudrad med florsocker och kanel – grillad frukt- sallad, vaniljglass,",
-    obsTitle: "",
+    obsTitle: "SÖTT",
   },
 ];
