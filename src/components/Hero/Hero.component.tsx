94187341d8521ae23edb36ccb32139c3937a855d
Nils Fält <nils.faelt@gmail.com>
2024-08-12 07:37:07 +0200

    removed sun from hero

--- src/components/Hero/Hero.component.tsx
+++ src/components/Hero/Hero.component.tsx
@@ -51,7 +51,7 @@ export const Hero: FC<Props> = () => {
               width={290}
               height={190}
             />
-            <TemporaryTextDisplay text="☀️" />
+            {/* <TemporaryTextDisplay text="☀️" /> */}
           </div>
         </Parallax>
       </ToogleVenueContainer>
